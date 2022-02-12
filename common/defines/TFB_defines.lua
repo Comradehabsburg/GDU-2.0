92da0a84ce9b943a8bd9e52cdaa28d32c5c06124
brennanthenerd <spiderkidfilms@gmail.com>
2022-02-11 23:11:55 -0800

    ai scrap out

--- common/defines/TFB_defines.lua
+++ common/defines/TFB_defines.lua
@@ -1,7 +1,7 @@
 
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 60				-- Days of client lag for decrease of gamespeed
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 60				-- Days of client lag for pause of gamespeed.
-NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.15, 0.07, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 400				-- Days of client lag for decrease of gamespeed
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 720				-- Days of client lag for pause of gamespeed.
+NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.2, 0.1, 0.03, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
 NDefines.NGame.EVENT_TIMEOUT_DEFAULT = 15						-- Default days before an event times out if not scripted
 NDefines.NGame.MISSION_REMOVE_FROM_INTERFACE_DEFAULT = 15		-- Default days before a mission is removed from the interface after having failed or completed
